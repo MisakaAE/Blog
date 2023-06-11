1701ee4b96c561102e8cc26ea22946e7ec846722
MisakaAE <2896264135@qq.com>
2023-06-11 14:57:41 +0800

    1

---
+++ Start.bat
@@ -0,0 +1,29 @@
+@echo off
+cd /d "%~dp0"
+title MENU
+
+:menu
+cls
+ECHO. [1]push origin [2]clean [3]server
+ECHO.
+echo.select£º
+set ID="q"
+set /p ID="> "
+if "%id%"=="1" goto cmd1
+if "%id%"=="2" goto cmd2
+if "%id%"=="3" goto cmd3
+if "%id%"=="q" goto end
+
+:end 
+exit
+
+:cmd1
+git push -u origin main
+
+
+:cmd2
+hexo clean
+
+
+:cmd3
+hexo server
